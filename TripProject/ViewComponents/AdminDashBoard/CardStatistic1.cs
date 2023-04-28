a63092fc9a0b14f6241cddf3cfac415971b694f3
Sevgül Ketenci <ketenci366660@gmail.com>
2023-04-28 23:36:02 +0300

    Admin Dashboard

---
+++ TripProject/ViewComponents/AdminDashBoard/CardStatistic1.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripProject.ViewComponents.AdminDashBoard
+{
+    public class CardStatistic1 : ViewComponent
+    {
+        Context connect = new Context();
+       
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.tourCount = connect.DestinationDbSet.Count();
+            ViewBag.userCount = connect.Users.Count();
+            return View();
+        }
+    }
+}
